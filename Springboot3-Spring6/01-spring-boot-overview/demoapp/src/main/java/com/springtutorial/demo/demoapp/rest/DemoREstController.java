7c0e8c32fc768639375d86f3fb613648b21f5817
michael apim <michael.apim@digital.homeoffice.gov.uk>
2024-05-13 16:08:28 +0100

    feat: complete spring tutorial section 1

--- Springboot3-Spring6/01-spring-boot-overview/demoapp/src/main/java/com/springtutorial/demo/demoapp/rest/DemoREstController.java
+++ Springboot3-Spring6/01-spring-boot-overview/demoapp/src/main/java/com/springtutorial/demo/demoapp/rest/DemoREstController.java
@@ -1,14 +1,32 @@
 package com.springtutorial.demo.demoapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController // specify it's a controller
-public class DemoREstController {
+public class DemoRestController {
+
+    @Value("${activity.name}")
+    private String activityName;
+
+    @Value("${activity.time}")
+    private String activityTime;
 
     @GetMapping("/")     // map requests based on endpoints defined
     public String sayHello(){
         return "Hello you!!!!";
+    }
+
+    @GetMapping("/dev")
+    public String sayDev(){
+        return "Dev Tools is up and running!!!";
+    }
+
+
+    @GetMapping("/workout")
+    public String workout(){
+        return "Run 1k today!!!" + activityName + " " + activityTime;
     }
 
 }
